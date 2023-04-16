b1b6df118075df10d7e468861b3974448d1ead24
kch0549 <kch0549@gmail.com>
2023-04-16 11:28:59 +0900

    test

---
+++ src/main/java/com/kandj/backoffice/service/PostsService.java
@@ -0,0 +1,52 @@
+package com.kandj.backoffice.service;
+
+import com.kandj.backoffice.domain.posts.Posts;
+import com.kandj.backoffice.domain.posts.PostsRepository;
+import com.kandj.backoffice.web.dto.PostsListResponseDto;
+import com.kandj.backoffice.web.dto.PostsResponseDto;
+import com.kandj.backoffice.web.dto.PostsSaveRequestDto;
+import com.kandj.backoffice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+}
---
+++ src/test/java/com/kandj/backoffice/web/dto/HelloResponseDTOTest.java
@@ -0,0 +1,21 @@
+package com.kandj.backoffice.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDTOTest {
+    @Test
+    public void lombok_test() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDTO dto = new HelloResponseDTO(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
